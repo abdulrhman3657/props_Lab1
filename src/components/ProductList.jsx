a20b5355b05fe466332a4f80f4f5248f21dc7743
user1 <user01@em/com>
2025-05-19 11:48:59 +0300

    c2

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -13,41 +13,17 @@ function ProductList(props) {
      ]
 
 
-    const listItems = products.map((person, key) =>
-        <li key={key}>{person}</li>
-    )
-
-    console.log(listItems)
-
   return (
     <>
     
-    {/* <ul style={{listStyle:"none"}} className="d-flex gap-3 flex-wrap">
+    <ul style={{listStyle:"none"}} className="d-flex gap-3 flex-wrap">
         <li ><Product name="Product 1" price="29.99" description="Description for Product 1" /></li>
         <li ><Product name="Product 2" price="39.99" description="Description for Product 2" /></li>
         <li ><Product name="Product 3" price="49.99" description="Description for Product 3" /></li>
         <li ><Product name="Product 4" price="59.99" description="Description for Product 4" /></li>
-    </ul> */}
-
-    {/* <ul style={{listStyle:"none"}} className="d-flex gap-3 flex-wrap">
-        {products.map(product => {
-            <li>{}</li>
-        })}
-    </ul> */}
-
-
-  <ul>
-    {
-        products.map(product, key) => {
-           return( <li key={key}>product</li>)
-        }
-    }
-    </ul>;
+    </ul>
     
     </>
-
-
-
   )
 }
 
